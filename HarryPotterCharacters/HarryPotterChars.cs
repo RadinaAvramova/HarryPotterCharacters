394def00c2d68177553cec45be6b80365669eb15
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-03-10 20:08:43 +0200

    Add files via upload

---
+++ HarryPotterCharacters/HarryPotterChars.cs
@@ -0,0 +1,20 @@
+﻿using CommandDotNet;
+using System.Linq;
+using CommandDotNet.Rendering;
+
+namespace HarryPotterCharacters
+{
+    public class HarryPotterChars
+    {
+        [SubCommand]
+        public Films Films { get; set; }
+
+        [Command(Description = "Lists the individual wizards")]
+        public void Wizards(IConsole console, string house = "")
+        {
+            var characters = Characters.GetCharacters()
+                .Where(wizard => wizard.House.Contains(house));
+            console.WriteTable(characters);
+        }
+    }
+}
